d485735d0374819927744702eb0372c038a20a67
thinkAmi <dev.thinkami@gmail.com>
2026-01-29 11:44:28 +0000

    feat: add session with redis cluster (redis gem 4.8.1)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,11 @@
 Rails.application.config.session_store :redis_store,
-                                       servers: [
-                                         {
-                                           host: "redis-single",
-                                           port: 6379,
-                                           db: 0,
-                                           namespace: "session"
-                                         }
-                                       ],
+                                       redis_store: Redis::Store.new(
+                                         cluster: %w[
+                                           redis://redis-cluster-node-1:6379
+                                           redis://redis-cluster-node-2:6379
+                                           redis://redis-cluster-node-3:6379
+                                         ],
+                                         namespace: "session"
+                                       ),
                                        expire_after: 90.minutes,
                                        key: "_redis_cluster_example_session"
